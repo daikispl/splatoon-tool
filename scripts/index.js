6aeb672b365cf23431251f975083cc4a7204aec0
s18130 <s18130@tokyo.kosen-ac.jp>
2021-11-23 20:57:33 +0900

    js,html

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,1 @@
 //index
-function changestage()
-{
-    document.getElementById("img").src = "/splatoon-tool/images/"+document.getElementById("selected").options[document.getElementById("selected").selectedIndex].text+".jpg";
-}
