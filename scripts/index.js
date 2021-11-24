b01e9382fba4f4ca19dc0252fad97db461c73535
s18130 <s18130@tokyo.kosen-ac.jp>
2021-11-24 21:25:10 +0900

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-//index
+function changestage()
+        {
+            var obj = document.getElementById("selected");
+            var idx = obj.selectedIndex;
+            var opt = document.getElementById(String(idx));
+            var txt = opt.value; 
+            var img = document.getElementById("imgs");
+            img.src = "/splatoon-tool/images/resize/"+txt+".jpg";
+        }
